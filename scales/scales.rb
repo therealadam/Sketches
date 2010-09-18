b54687473334c34e494751e1fccd5d557df565a8
Adam Keys <adam@therealadam.com>
2010-09-18 11:10:04 -0500

    Support caching/fetching on multiple keys.

--- scales/scales.rb
+++ scales/scales.rb
@@ -1,4 +1,5 @@
 require "rubygems"
+require "active_support/core_ext/class"
 require "active_support/core_ext/module"
 require "active_support/concern"
 
@@ -10,12 +11,13 @@ module Scales
     extend ActiveSupport::Concern
     
     included do
+      
       def self.cache_by(name)
-        # add callbacks to populate, update, and clear the cache
+        caches << name
       end
       
-      def self.get(*args)
-        Scales.memcache.get(key_for(args.first))
+      def self.get(fragment, key)
+        Scales.memcache.get(key_for(key, fragment))
       end
       
       def self.key_for(identifier, fragments=[])
@@ -25,16 +27,19 @@ module Scales
         key.flatten.join(":")
       end
       
+      cattr_accessor(:caches) { [] } # Less ugly way to do this?
+      
       after_create :save_to_cache
-      after_update :update_cache
-      after_destroy :delete_from_cache
+      after_update :update_cache # TODO
+      after_destroy :delete_from_cache # TODO
     end
     
     protected
     
     def save_to_cache
-      # Possible to use JSON?
-      Scales.memcache.set(self.class.key_for(id), self)
+      self.class.caches.each do |key|
+        Scales.memcache.set(self.class.key_for(self.send(key), key), self)
+      end
     end
     
   end
@@ -67,8 +72,9 @@ class User < ActiveRecord::Base
 end
 
 Scales.memcache = Memcached.new("localhost") # Support Moneta someday?
+Scales.memcache.flush
 
 users = %w{peter ray egon winston}.map { |u| User.create(:username => u ) }
 
-p User.get(1)
-# User.get(:username => "peter")
+p User.get(:id, 2)
+p User.get(:username, "peter")
